49195f1ee915877bf8f87f7b5ac7b116e11cbd35
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-11-06 05:29:37 +0000

    Deploy v-scroll-lock.peterhegman.com to github.com/phegman/v-scroll-lock.git:gh-pages

---
+++ interfaces/plugin-options.interface.d.ts
@@ -0,0 +1,5 @@
+import { disableBodyScroll, enableBodyScroll } from 'body-scroll-lock';
+export default interface PluginOptions {
+    disableBodyScroll?: typeof disableBodyScroll;
+    enableBodyScroll?: typeof enableBodyScroll;
+}
